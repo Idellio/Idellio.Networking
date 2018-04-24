6ff5e1b36e6620520f349a28618400eb7d6cda7d
Nick Brown <njbgaming09@gmail.com>
2018-04-24 19:42:17 -0400

    Splitting receive packets

--- Idellio.Networking/MonoBehaviours/BaseNetworkServer.cs
+++ Idellio.Networking/MonoBehaviours/BaseNetworkServer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Idellio.Networking.Packets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,7 @@ namespace Idellio.Networking.MonoBehaviours
 
         private List<BaseNetworkBehaviour> _NetworkBehaviours { get; set; }
         private List<NetworkConnection> _Connections { get; set; }
+        private List<NetworkMessage> _IncomingPackets { get; set; }
 
 
         void Awake()
@@ -103,32 +105,38 @@ namespace Idellio.Networking.MonoBehaviours
         private void ServerTick()
         {
             if (_TimeSinceLastTick >= (1 / (float)TickRate)) {
-                if (!Hosting || !Initialized) throw new Exception("[IDNS-Server] Error Running Server Tick.");
-                byte err;
-                int connectionId;
-                int channelId;
-                byte[] recBuffer = new byte[MaxPacketSize];
-                int bufferSize = MaxPacketSize;
-                int dataSize;
-                byte error;
-                int size = NetworkTransport.GetIncomingMessageQueueSize(_HostId, out err) + 1;
-                for (int i = 0; i < size; i++)
-                {
-                    NetworkEventType recData = NetworkTransport.ReceiveFromHost(_HostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
-                    switch (recData)
-                    {
-                        case NetworkEventType.Nothing:         //1
-                            break;
-                        case NetworkEventType.ConnectEvent:    //2
-                            _Connections.Add(new NetworkConnection(connectionId));
-                            break;
-                        case NetworkEventType.DataEvent:       //3
-                            break;
-                        case NetworkEventType.DisconnectEvent: //4
-                            break;
-                    }
-                }
+                ReceiveIncomingPackets();
                 _TimeSinceLastTick = 0f;
+            }
+        }
+
+        private void ReceiveIncomingPackets()
+        {
+            if (!Hosting || !Initialized) throw new Exception("[IDNS-Server] Error Running Server Tick.");
+            _IncomingPackets = new List<NetworkMessage>();
+            byte err;
+            int connectionId;
+            int channelId;
+            byte[] recBuffer = new byte[MaxPacketSize];
+            int bufferSize = MaxPacketSize;
+            int dataSize;
+            byte error;
+            int size = NetworkTransport.GetIncomingMessageQueueSize(_HostId, out err) + 1;
+            for (int i = 0; i < size; i++)
+            {
+                NetworkEventType recData = NetworkTransport.ReceiveFromHost(_HostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+                switch (recData)
+                {
+                    case NetworkEventType.Nothing:         //1
+                        break;
+                    case NetworkEventType.ConnectEvent:    //2
+                        _Connections.Add(new NetworkConnection(connectionId));
+                        break;
+                    case NetworkEventType.DataEvent:       //3
+                        break;
+                    case NetworkEventType.DisconnectEvent: //4
+                        break;
+                }
             }
         }
 
---
+++ Idellio.Networking/Packets/NetworkMessage.cs
@@ -0,0 +1,18 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Idellio.Networking.Packets
+{
+    [MessagePackObject]
+    class NetworkMessage
+    {
+        [Key(0)]
+        public string Type { get; set; }
+
+        [Key(1)]
+        public byte[] Buffer { get; set; }
+    }
+}
